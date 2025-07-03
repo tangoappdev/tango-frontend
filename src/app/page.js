2865da1a78fa4c740dc1aa921c83b04a8cf24054
Rainier Pereira <your@email.com>
2025-07-03 13:02:17 -0400

    fix: background color

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import TangoPlayer from '@/components/TangoPlayer';
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 bg-gray-800">
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 bg-[#30333a]">
       <TangoPlayer />
     </main>
   );
