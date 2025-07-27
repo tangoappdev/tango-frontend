cc96333644a6a75307faf8f71be501645aed6a7b
Rainier Pereira <your@email.com>
2025-07-27 10:37:14 -0700

    Add new changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import TangoPlayer from '@/components/TangoPlayer';
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 bg-[#30333a]">
+    <main className="flex min-h-screen flex-col items-center justify-center p-2 sm:p-8 bg-[#30333a]">
       <TangoPlayer />
     </main>
   );
