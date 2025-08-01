8baa46da411ae1771ff362bead552152da31be91
Rainier Pereira <your@email.com>
2025-08-01 10:22:57 -0400

    Add new changes

--- src/components/TangoPlayer.js
+++ src/components/TangoPlayer.js
@@ -680,7 +680,7 @@ export default function TangoPlayer() {
     };
 
     return (
-        <div className="p-2 sm:p-4 flex justify-center items-start">
+        <div className="p-2 sm:p-1 flex justify-center items-start">
             <div className="p-2 bg-transparent text-white rounded-lg w-full max-w-[32rem] font-sans">
                 {menuState.visible && (
                     <ContextMenu
