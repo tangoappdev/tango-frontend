f7ccbcad78fd71f51081bcf93670a81314c94436
Rainier Pereira <your@email.com>
2025-08-11 13:09:31 -0400

    Checkpoint: App is working before admin refactor

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../../lib/firebaseClient';
+import { auth } from '../../../lib/firebaseClient';
 
 export default function AdminLayout({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
